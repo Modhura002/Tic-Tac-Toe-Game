1a80fa3d2603e7d83b661859d0916f64763a103e
Modhura002 <modhura.banerjee@gmail.com>
2025-07-02 15:45:55 +0530

    modification on logic

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let resetbtn=document.querySelector(".reset");
 let newbtn=document.querySelector(".new");
 const winComb=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 let playerO=true;
+
 boxes.forEach((box)=>{
     box.addEventListener("click",()=>{
         if(box.innerText!=="")
@@ -22,6 +23,7 @@ boxes.forEach((box)=>{
     });
 
 });
+
 const checkWinner=()=>{
     for(let pos of winComb){
         let p1=boxes[pos[0]].innerText;
@@ -32,19 +34,21 @@ const checkWinner=()=>{
                
                 let winner=p1;
                 console.log(winner+" wins");
+                announceWinner(winner);
                  disable();
-                 return;
-
-                
+                 return;  
             }
                 
         }
     }
-
-
 };
+
 const disable=()=>{
     boxes.forEach((box)=>{
         box.disabled=true;
     });
+}
+
+const announceWinner=(winner)=>{
+
 }
