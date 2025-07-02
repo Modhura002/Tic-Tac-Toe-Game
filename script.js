b3c6bbb554753ef5643bf50d65edee4807dbbfb9
Modhura002 <modhura.banerjee@gmail.com>
2025-07-02 12:20:53 +0530

    JS added

---
+++ script.js
@@ -0,0 +1,4 @@
+let boxes=document.querySelectorAll(".squares");
+let resetbtn=document.querySelector(".reset");
+let newbtn=document.querySelector(".new");
+let winComb=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
